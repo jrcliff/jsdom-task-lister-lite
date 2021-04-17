fba4bf973838217b01dfc224bd22c74584d2abc0
jrcliff <justoon7.jc@gmail.com>
2021-04-17 15:52:47 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+    document.querySelector('form').addEventListener('submit', (event) => {
+      event.preventDefault();
+      let newTask = document.getElementById('new-task-description')
+      console.log(newTask.value);
+      addTask()
+
+  })
+  
 });
+
+function addTask(){
+  let ul = document.getElementById('tasks')
+  let listItem = document.createElement('li')
+  listItem.innerText = document.getElementById('new-task-description').value
+  // listItem.appendChild(listItem) 
+  ul.appendChild(listItem)
+
+
+
+}
